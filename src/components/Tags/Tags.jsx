6540ebfefa2220a4baaa554bbf0116c24238c0c2
Nicolas Martinez <nicmart@gmail.com>
2024-05-23 16:31:22 -0700

    pass status text properly to display and apply matching style

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,7 +1,14 @@
 import "./Tags.scss";
-
 import React from "react";
 
-const Tags = ({ status }) => <h4 className={`tag tag--${status}`}>in stock</h4>;
+// Function to remove space and lower case status values "In Stock" and "Out of Stock" to use as classname
+function formatStatus(input) {
+  return input.replace(/\s+/g, "").toLowerCase();
+}
+
+const Tags = ({ status }) => {
+  const formattedStatus = formatStatus(status);
+  return <h4 className={`tag tag--${formattedStatus}`}>{status}</h4>;
+};
 
 export default Tags;
