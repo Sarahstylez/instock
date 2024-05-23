47e3f5cbe7483d839473e4e09252f1f18c11ef55
maryxm9 <maryxm9@hotmail.com>
2024-05-24 06:15:03 +0800

    remove api from axios

--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,13 +1,40 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import "./Warehouse.scss";
 
-const WarehousePage = () => {
+const WarehouseList = () => {
+  const [warehouses, setWarehouses] = useState([]);
+
+  useEffect(() => {
+    const fetchWarehouses = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/warehouses');
+        setWarehouses(response.data);
+      } catch (error) {
+        console.error("Error fetching warehouse data:", error);
+      }
+    };
+
+    fetchWarehouses();
+  }, []);
+
   return (
-    <>
-      <h1>Workout Page</h1>
-    </>
+    <section className="warehouse__section">
+      <h1 className="warehouse__title">Warehouses</h1>
+
+      <ul>
+        {warehouses.map((warehouse) => (
+          <li key={warehouse.id}>
+            <h2>{warehouse.warehouse_name}</h2>
+            <p>{warehouse.address}, {warehouse.city}, {warehouse.country}</p>
+            <p>{warehouse.contact_name} - {warehouse.contact_position}</p>
+            <p>{warehouse.contact_phone}</p>
+            <p>{warehouse.contact_email}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
-export default WarehousePage;
+export default WarehouseList;
