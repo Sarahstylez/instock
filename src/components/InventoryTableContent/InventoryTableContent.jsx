be195d5373079983ee2459527a37f77baf6bf727
Nicolas Martinez <nicmart@gmail.com>
2024-05-24 16:18:36 -0700

    rename inventory table component

---
+++ src/components/InventoryTableContent/InventoryTableContent.jsx
@@ -0,0 +1,56 @@
+import "./InventoryTableContent.scss";
+
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
+import chevronIcon from "../../assets/Icons/chevron_right-24px.svg";
+
+const InventoryTableContent = ({ page, listItem }) => {
+  return (
+    <>
+      <div className="mobile-warehouse">
+        <div className="mobile-warehouse-title">WAREHOUSE</div>
+        <div className="list-table__name">
+          {listItem.warehouse_name}
+          <img
+            className="list-table__icon"
+            src={chevronIcon}
+            alt="chevron icon"
+          />
+        </div>
+      </div>
+
+      <div className="mobile-add">
+        <div className="mobile-add-title">ADDRESS</div>
+        <div className="list-table__add">
+          {listItem.address} {listItem.city}, {listItem.country}
+        </div>
+      </div>
+
+      <div className="mobile-contact-name">
+        <div className="mobile-contact-name-title">CONTACT NAME</div>
+        <div className="list-table__contact-name">{listItem.contact_name}</div>
+      </div>
+
+      <div className="mobile-contact-info">
+        <div className="mobile-contact-info-title">CONTACT INFORMATION</div>
+        <div className="list-table__contact-info">
+          <p>{listItem.contact_phone}</p>
+          <p>{listItem.contact_email}</p>
+        </div>
+      </div>
+
+      <div className="list-table__actions-delete">
+        <img
+          className="list-table__delete"
+          src={deleteIcon}
+          alt="delete warehouse"
+        />
+      </div>
+      <div className="list-table__actions-edit">
+        <img className="list-table__edit" src={editIcon} alt="edit warehouse" />
+      </div>
+    </>
+  );
+};
+
+export default InventoryTableContent;
--- src/components/ListTable/ListTable.jsx
+++ src/components/ListTable/ListTable.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "./ListTable.scss";
-import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
-import editIcon from "../../assets/Icons/edit-24px.svg";
-import chevronIcon from "../../assets/Icons/chevron_right-24px.svg";
 
 import PageHeader from "../PageHeader/PageHeader";
 import TableHeader from "../TableHeader/TableHeader";
+import InventoryTableContent from "../InventoryTableContent/InventoryTableContent";
 
 const ListTable = ({ page }) => {
   const [list, setList] = useState([]);
@@ -39,7 +37,7 @@ const ListTable = ({ page }) => {
     setSortConfig({ key, direction });
   };
 
-  const sortedWarehouses = [...list].sort((a, b) => {
+  const sortedLists = [...list].sort((a, b) => {
     if (a[sortConfig.key] < b[sortConfig.key]) {
       return sortConfig.direction === "ascending" ? -1 : 1;
     }
@@ -55,58 +53,9 @@ const ListTable = ({ page }) => {
         <PageHeader page={page} />
         <ul>
           <TableHeader page={page} sortItems={sortItems} />
-          {sortedWarehouses.map((warehouse) => (
-            <li className="list-table__list" key={warehouse.id}>
-              <div className="mobile-warehouse">
-                <div className="mobile-warehouse-title">WAREHOUSE</div>
-                <div className="list-table__name">
-                  {warehouse.warehouse_name}
-                  <img
-                    className="list-table__icon"
-                    src={chevronIcon}
-                    alt="chevron icon"
-                  />
-                </div>
-              </div>
-
-              <div className="mobile-add">
-                <div className="mobile-add-title">ADDRESS</div>
-                <div className="list-table__add">
-                  {warehouse.address} {warehouse.city}, {warehouse.country}
-                </div>
-              </div>
-
-              <div className="mobile-contact-name">
-                <div className="mobile-contact-name-title">CONTACT NAME</div>
-                <div className="list-table__contact-name">
-                  {warehouse.contact_name}
-                </div>
-              </div>
-
-              <div className="mobile-contact-info">
-                <div className="mobile-contact-info-title">
-                  CONTACT INFORMATION
-                </div>
-                <div className="list-table__contact-info">
-                  <p>{warehouse.contact_phone}</p>
-                  <p>{warehouse.contact_email}</p>
-                </div>
-              </div>
-
-              <div className="list-table__actions-delete">
-                <img
-                  className="list-table__delete"
-                  src={deleteIcon}
-                  alt="delete warehouse"
-                />
-              </div>
-              <div className="list-table__actions-edit">
-                <img
-                  className="list-table__edit"
-                  src={editIcon}
-                  alt="edit warehouse"
-                />
-              </div>
+          {sortedLists.map((listItem) => (
+            <li className="list-table__list" key={listItem.id}>
+              <InventoryTableContent page={page} listItem={listItem} />
             </li>
           ))}
         </ul>
