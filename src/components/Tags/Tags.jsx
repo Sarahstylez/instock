4ca2aedc6df57940108feaa4ac27ad297ab8a892
Nicolas Martinez <nicmart@gmail.com>
2024-05-23 21:53:14 -0700

    add comments on how to use Tags component

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -6,6 +6,11 @@ function formatStatus(input) {
   return input.replace(/\s+/g, "").toLowerCase();
 }
 
+/* Show in stock or out of stock tag
+Component usage:
+  <Tags status="In Stock" />
+  <Tags status="Out Of Stock" />
+*/
 const Tags = ({ status }) => {
   const formattedStatus = formatStatus(status);
   return <h4 className={`tag tag--${formattedStatus}`}>{status}</h4>;
