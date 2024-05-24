bf87da13c3744cd2d0e57d6191fb865a8feb05ca
Nicolas Martinez <nicmart@gmail.com>
2024-05-24 10:42:32 -0700

    refactor moving code from TablesInventory to InventoryItem

--- src/components/TablesInventory/TablesInventory.jsx
+++ src/components/TablesInventory/TablesInventory.jsx
@@ -1,55 +1,12 @@
 import "./TablesInventory.scss";
-import Chevron from "../../assets/Icons/chevron_right-24px.svg";
-import TrashBin from "../../assets/Icons/delete_outline-24px.svg";
-import EditButton from "../../assets/Icons/edit-24px.svg";
-import Tags from "../Tags/Tags";
+import InventoryItem from "../InventoryItem/InventoryItem";
 
 /* TODO update table and use GET API endpoint */
 function TablesInventory() {
   return (
-    <div className="inventory">
-      <div className="inventory__text">
-        <div className="inventory__container">
-          <div className="inventory__item">
-            <h4 className="inventory__item-title">Inventory Item</h4>
-            <div className="inventory__item-item">
-              <h3>Television</h3>
-              <img
-                className="inventory__item-icon"
-                src={Chevron}
-                alt="To Item Details"
-              ></img>
-            </div>
-          </div>
-          <div className="inventory__category">
-            <h4 className="inventory__category-title">Category</h4>
-            <p className="inventory__category-text p2">Electronics</p>
-          </div>
-        </div>
-        <div className="inventory__container">
-          <div className="inventory__status">
-            <h4 className="inventory__status-title">Status</h4>
-            <div className="inventory__status-tag">
-              {/* TODO replace by correct status field from api endpoint */}
-              <Tags status="In Stock" />
-              <Tags status="Out Of Stock" />
-            </div>
-          </div>
-          <div className="inventory__quantity">
-            <h4 className="inventory__quantity-title">Qty</h4>
-            <p className="inventory__quantity-value p2">500</p>
-          </div>
-        </div>
-      </div>
-      <div className="inventory__icons">
-        <img
-          className="inventory__delete"
-          src={TrashBin}
-          alt="Delete Item"
-        ></img>
-        <img className="inventory__edit" src={EditButton} alt="Edit Item"></img>
-      </div>
-    </div>
+    <>
+      <InventoryItem />
+    </>
   );
 }
 
