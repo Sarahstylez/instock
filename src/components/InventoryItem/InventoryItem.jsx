4e1795f9a71c50d44c8e2bde1a4a0b700854286e
Nicolas Martinez <nicmart@gmail.com>
2024-05-24 11:17:54 -0700

    use intentory item component to display single item

--- src/components/InventoryItem/InventoryItem.jsx
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -3,41 +3,53 @@ import Chevron from "../../assets/Icons/chevron_right-24px.svg";
 import TrashBin from "../../assets/Icons/delete_outline-24px.svg";
 import EditButton from "../../assets/Icons/edit-24px.svg";
 import Tags from "../Tags/Tags";
+import { Link } from "react-router-dom";
 
-/* TODO update table and use GET API endpoint */
-function InventoryItem() {
+/* Return a single inventory item, allow to click to navigate to see inventory item details */
+function InventoryItem({
+  id,
+  name,
+  status,
+  category,
+  quantity,
+  warehouse_name,
+}) {
   return (
     <div className="inventory">
       <div className="inventory__text">
         <div className="inventory__container">
           <div className="inventory__item">
             <h4 className="inventory__item-title">Inventory Item</h4>
-            <div className="inventory__item-item">
-              <h3>Television</h3>
-              <img
-                className="inventory__item-icon"
-                src={Chevron}
-                alt="To Item Details"
-              ></img>
-            </div>
+            <Link to={`/inventory/${id}`}>
+              <div className="inventory__item-item">
+                <h3>{name}</h3>
+                <img
+                  className="inventory__item-icon"
+                  src={Chevron}
+                  alt="To Item Details"
+                ></img>
+              </div>
+            </Link>
           </div>
           <div className="inventory__category">
             <h4 className="inventory__category-title">Category</h4>
-            <p className="inventory__category-text p2">Electronics</p>
+            <p className="inventory__category-text p2">{category}</p>
           </div>
         </div>
         <div className="inventory__container">
           <div className="inventory__status">
             <h4 className="inventory__status-title">Status</h4>
             <div className="inventory__status-tag">
-              {/* TODO replace by correct status field from api endpoint */}
-              <Tags status="In Stock" />
-              <Tags status="Out Of Stock" />
+              <Tags status={status} />
             </div>
           </div>
           <div className="inventory__quantity">
             <h4 className="inventory__quantity-title">Qty</h4>
-            <p className="inventory__quantity-value p2">500</p>
+            <p className="inventory__quantity-value p2">{quantity}</p>
+          </div>
+          <div className="inventory__warehouse">
+            <h4 className="inventory__warehouse-title">Warehouse</h4>
+            <p className="inventory__warehouse-value p2">{warehouse_name}</p>
           </div>
         </div>
       </div>
--- src/components/TablesInventory/TablesInventory.jsx
+++ src/components/TablesInventory/TablesInventory.jsx
@@ -3,9 +3,36 @@ import InventoryItem from "../InventoryItem/InventoryItem";
 
 /* TODO update table and use GET API endpoint */
 function TablesInventory() {
+  const items = [
+    {
+      id: 9,
+      warehouse_name: "Manhattan",
+      item_name: "Television",
+      description:
+        'This 50", 4K LED TV provides a crystal-clear picture and vivid colors.',
+      category: "Electronics",
+      status: "In Stock",
+      quantity: 500,
+    },
+    // Add more items here
+  ];
+
   return (
     <>
-      <InventoryItem />
+      <div className="inventory__page">
+        {/* Map through list and on click navigate item details page */}
+        {items.map((item) => (
+          <InventoryItem
+            key={item.id}
+            id={item.id} // Pass the id prop for navigation
+            name={item.item_name}
+            status={item.status}
+            category={item.category}
+            quantity={item.quantity}
+            warehouse_name={item.warehouse_name}
+          />
+        ))}
+      </div>
     </>
   );
 }
