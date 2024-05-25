6d33a245c55d33a3f3654ace64805c6d443f5b88
Nicolas Martinez <nicmart@gmail.com>
2024-05-24 23:59:19 -0700

    cleanup inventory page and deprecate inventoryitem and tablesinventory components

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,15 +1,9 @@
 import "./Inventory.scss";
 import ListTable from "../../components/ListTable/ListTable";
 
-//importing relevant components to display on page
-import TablesInventory from "../../components/TablesInventory/TablesInventory";
-
 const InventoryPage = () => {
   return (
     <>
-      {/* <TablesInventory /> */}
-      {/* <ListTable page={"inventory"} /> */}
-
       <ListTable page={"inventory"} />
     </>
   );
