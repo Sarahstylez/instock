76c78e6c49226a5115ae172ed16ed9440320bc41
Nicolas Martinez <nicmart@gmail.com>
2024-05-26 16:58:13 -0700

    make footer always visible on mobile, making different design choice than mockup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,8 @@
 import "./Footer.scss";
-import React, { useEffect, useState } from "react";
 
 function Footer() {
-  const [isVisible, setIsVisible] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const windowHeight = window.innerHeight;
-      const bodyHeight = document.body.offsetHeight;
-      const footer = document.querySelector(".footer");
-
-      // Check if the user has scrolled to the bottom of the page
-      if (windowHeight + window.scrollY >= bodyHeight) {
-        setIsVisible(false);
-        footer.style.transform = "translateY(100%)"; // Hide footer
-      } else {
-        setIsVisible(true);
-        footer.style.transform = "translateY(0)"; // Show footer
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <footer className={`footer ${isVisible ? "visible" : "hidden"}`}>
+    <footer className={"footer"}>
       <p>© InStock Inc. All Rights Reserved.</p>
     </footer>
   );
