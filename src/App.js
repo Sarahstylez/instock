7fda3ee5c4255993edd765b22f42c9fa1cbaae6b
Sarahstylez <sarahgee@my.capilanou.ca>
2024-05-31 17:38:35 -0700

    add HashRouter and install gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import InventoryPage from "./pages/Inventory/Inventory";
 import WarehousePage from "./pages/Warehouse/Warehouse";
 import EditItemPage from "./pages/EditItem/EditItem";
@@ -12,30 +12,29 @@ import Footer from "./components/Footer/Footer";
 import EditWarehouse from "./components/EditWarehouse/EditWarehouse";
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-                {<Navigation />}
+  return (
+    <div className="App">
+      <HashRouter>
+        {<Navigation />}
 
-
-                <div className="App__routes-overlay">
-                    <Routes>
-                        <Route path="/" element={<WarehousePage />} />
-                        <Route path="/inventory" element={<InventoryPage />} />
-                        <Route path="/warehouses" element={<WarehousePage />} />
-                        <Route path="/inventory/new" element={<AddInventoryItem />} />
-                        <Route path="/warehouses/new" element={<AddWarehouse />} />
-                        <Route path="/inventory/:id" element={<InventoryDetailsCard />} />
-                        <Route path="/warehouses/:id" element={<WarehouseDetails />} />
-                        <Route path="/inventory/:id/edit/" element={<EditItemPage />} />
-                        <Route path="/warehouses/:id/edit" element={<EditWarehouse />} />
-                        <Route path="*" element={<h1>Page not found</h1>} />
-                    </Routes>
-                </div>
-                <Footer />
-            </BrowserRouter>
+        <div className="App__routes-overlay">
+          <Routes>
+            <Route path="/" element={<WarehousePage />} />
+            <Route path="/inventory" element={<InventoryPage />} />
+            <Route path="/warehouses" element={<WarehousePage />} />
+            <Route path="/inventory/new" element={<AddInventoryItem />} />
+            <Route path="/warehouses/new" element={<AddWarehouse />} />
+            <Route path="/inventory/:id" element={<InventoryDetailsCard />} />
+            <Route path="/warehouses/:id" element={<WarehouseDetails />} />
+            <Route path="/inventory/:id/edit/" element={<EditItemPage />} />
+            <Route path="/warehouses/:id/edit" element={<EditWarehouse />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
         </div>
-    );
+        <Footer />
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
